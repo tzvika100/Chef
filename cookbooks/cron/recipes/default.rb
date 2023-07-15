12be039a6c5de862b47bab03c6266201591fd318
tzvika100 <37474416+tzvika100@users.noreply.github.com>
2023-07-15 17:36:51 +0300

    Update default.rb
    fixed path

--- cookbooks/cron/recipes/default.rb
+++ cookbooks/cron/recipes/default.rb
@@ -5,8 +5,8 @@
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
 cron 'runinng_main' do
-       minute '*/1'
+       minute '*/2'
        user 'root'
-       command 'bash /home/ubuntu/chef/cookbook/cron/files/cron.sh'
+       command 'bash /home/ubuntu/chef/cookbooks/cron/files/cron.sh'
        action :create
 end
