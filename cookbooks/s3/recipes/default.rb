9076afaf705720cb9c30a7979523f045ded298e4
tzvika100 <37474416+tzvika100@users.noreply.github.com>
2023-07-15 12:08:41 +0300

    Update default.rb
    fixed: var +condition

--- cookbooks/s3/recipes/default.rb
+++ cookbooks/s3/recipes/default.rb
@@ -6,22 +6,23 @@
 
 version = node["s3"]["version"]
 
-if Dir.exist?('/home/ubuntu/app')
+if !Dir.exist?('/home/ubuntu/app')
 	directory '/home/ubuntu/app' do
 		recursive true
 		action :create
 	end
+end
 
+if Dir.exist?('/home/ubuntu/app')
 	execute 'clean old version' do
         	command 'rm -r /home/ubuntu/app/*.rar'
     		ignore_failure true
   	end
-
 end
 
 remote_file_s3 '/home/ubuntu/app/apk' do
 	bucket 'my-storage-apk'
-        remote_path 'app/'
+        remote_path 'app/#{version}.tar'
         aws_access_key_id 'AKIAWYQUEW3AR3ZX2X2G'
         aws_secret_access_key '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'
         region 'us-west-2'
