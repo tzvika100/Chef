2b658175e1f542eab35cbbaeff61fabbb1c9c8a9
tzvika100 <37474416+tzvika100@users.noreply.github.com>
2023-07-15 13:33:38 +0300

    Update default.rb
    syntax again

--- cookbooks/s3/recipes/default.rb
+++ cookbooks/s3/recipes/default.rb
@@ -22,7 +22,7 @@ end
 
 remote_file_s3 "/home/ubuntu/app/#{version}.tar" do
 	bucket 'my-storage-apk'
-        remote_path 'app/#{version}.tar'
+        remote_path "app/#{version}.tar"
         aws_access_key_id 'AKIAWYQUEW3AR3ZX2X2G'
         aws_secret_access_key '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'
         region 'us-west-2'
@@ -30,7 +30,7 @@ remote_file_s3 "/home/ubuntu/app/#{version}.tar" do
 end
 
 execute 'extract_artifact' do
-  command "tar -xf /home/tzvi/app/#{version}.tar "
+  command "tar -xf /home/tzvi/app/#{version}.tar"
   action :run
 end
 
